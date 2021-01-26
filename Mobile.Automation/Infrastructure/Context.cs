680e7867cfa4f4bb5885f2fe6a85a292aea72bc5
saransh vaid <saranshvaid8@gmail.com>
2021-01-26 12:39:13 -0500

    Add project files.

---
+++ Mobile.Automation/Infrastructure/Context.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.Service;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace Automation.Framework.Mobile.Automation.Infrastructure
+{
+    /// <summary>
+    /// This class contains setup context
+    /// </summary>
+    public class Context
+    {
+        #region Private Members
+        private const string _applicationUnderTest = "appPath";
+        
+        private AppiumDriver<AndroidElement> AndroidContext;
+        public AppiumLocalService AppiumServiceContext;
+
+        #endregion
+
+        public IDictionary<string, object> Configuration;
+
+        /// <summary>
+        /// Set's up the overall initial state i.e.
+        /// starting appiumServer, getting configurations and initializing the driver
+        /// </summary>
+        public void SetUp()
+        {
+            
+            this.GetConfiguration();
+            
+        }
+
+
+        
+        /// <summary>
+        /// Closes active appium session
+        /// </summary>
+        public void TearDown()
+        {
+            AppiumServiceContext.Dispose();
+        }
+
+
+
+        /// <summary>
+        /// Initializes the IOS drivert to launch the app
+        /// </summary>
+        private void GetIosDriver()
+        {
+            // initialize options
+            //add capabilities
+            //initialize IOS driver
+        }
+        /// <summary>
+        /// Reads the "Config.Json" from the output directory and returns a Dictionary 
+        /// </summary>
+        /// <returns>IDictionary<string,object></string></returns>
+        public IDictionary<string, object> GetConfiguration()
+        {
+            //string fileName = "Config.Json";
+            //string path = Path.Combine(Environment.CurrentDirectory,@"Infrastructure" , fileName);
+
+            var text = File.ReadAllText(@"S:\QA\Automation\Appium\src\Automation.Framework\Mobile.Automation\Infrastructure\Config.Json");
+            Configuration = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(text);
+            
+            return Configuration;
+
+        }
+    }
+}
---
+++ Mobile.Automation/Utilities/AppiumUtilities.cs
@@ -0,0 +1,79 @@
+﻿using Automation.Framework.Mobile.Automation.Infrastructure;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.Service;
+
+namespace Automation.Framework.Mobile.Automation.Utilities
+{
+    /// <summary>
+    ///The AppiumUtilities class
+    ///Contains methods pertaining to Appium
+    ///e.g.Method to start AppiumService locally
+    /// </summary>
+    /// 
+    ///
+    /// <remarks>
+    /// Before using the Utilities please ensure Appium is installed.
+    /// Please see <see cref="http://appium.io/docs/en/about-appium/getting-started/?lang=en"/>
+    /// </remarks>
+    public class AppiumUtilities : Context
+    { 
+        #region Private Members
+
+        private static AppiumLocalService _appiumLocalService;
+        private const string _applicationUnderTest = "appPath";
+
+        #endregion Private Members
+
+        #region Methods
+
+        /// <summary>
+        /// Starts Appium Server locally on available free port
+        /// </summary>
+        public AppiumLocalService StartAppiumLocalService()
+        {
+            _appiumLocalService = new AppiumServiceBuilder().UsingAnyFreePort().Build();
+            if (!_appiumLocalService.IsRunning)
+                _appiumLocalService.Start();
+
+            return _appiumLocalService;
+        }
+
+        /// <summary>
+        /// Starts Appium Server locally.
+        /// </summary>
+        /// <param name="portNumber"></param>
+        /// <returns></returns>
+        public AppiumLocalService StartAppiumLocalService(int portNumber)
+        {
+            _appiumLocalService = new AppiumServiceBuilder().UsingPort(portNumber).Build();
+            if (!_appiumLocalService.IsRunning)
+                _appiumLocalService.Start();
+
+            return _appiumLocalService;
+        }
+
+        public static AndroidDriver<AndroidElement> InitializeAndroidNativeApp()
+        {
+            Context context = new Context();
+            var driverOptions = new AppiumOptions();
+            context.GetConfiguration();
+            dynamic devicesFromconfig = context.Configuration["devices"];
+            driverOptions.AddAdditionalCapability(MobileCapabilityType.PlatformName, devicesFromconfig[0].os);
+            driverOptions.AddAdditionalCapability(MobileCapabilityType.App, context.Configuration[_applicationUnderTest]);
+            driverOptions.AddAdditionalCapability(MobileCapabilityType.DeviceName, devicesFromconfig[0].deviceName);
+            driverOptions.AddAdditionalCapability("autoAcceptAlerts", true);
+            driverOptions.AddAdditionalCapability(MobileCapabilityType.NewCommandTimeout, 100);
+            driverOptions.AddAdditionalCapability("autoGrantPermissions", true);
+
+            //This line is specific to the current AUT
+            driverOptions.AddAdditionalCapability("appWaitActivity", "md525147372f3ece587b62293ecc226e68b.MainActivity");
+
+            AndroidDriver<AndroidElement> androidDriver = new AndroidDriver<AndroidElement>(_appiumLocalService, driverOptions);
+            return androidDriver;
+        }
+
+        #endregion Methods
+    }
+}
